e33e61131ba1f691b6a9c38f19c2de2d30013a01
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-09-18 00:48:44 +0530

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { Refine, Authenticated } from "@refinedev/core";
 import routerProvider from "@refinedev/react-router-v6";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 
 import { dataProvider } from "./providers/data-provider";
 import { authProvider } from "./providers/auth-provider";
@@ -16,14 +22,32 @@ import { Header } from "./components/header";
 export default function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <Refine dataProvider={dataProvider} authProvider={authProvider}>
-        <Authenticated key="protected" fallback={<Login />}>
-          <Header />
-          {/* <ShowProduct /> */}
-          {/* <EditProduct /> */}
-          <ListProducts />
-          {/* <CreateProduct /> */}
-        </Authenticated>
+      <Refine
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        routerProvider={routerProvider}
+      >
+        <Routes>
+          <Route
+            element={
+              <Authenticated key="authenticated-routes" redirectOnFail="/login">
+                <Header />
+                <Outlet />
+              </Authenticated>
+            }
+          >
+            <Route index element={<ListProducts />} />
+          </Route>
+          <Route
+            element={
+              <Authenticated key="auth-pages" fallback={<Outlet />}>
+                <Navigate to="/" />
+              </Authenticated>
+            }
+          >
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
       </Refine>
     </BrowserRouter>
   );
