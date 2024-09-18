e395edb6834215af9526570fb44ae6f2c2626957
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-09-18 22:27:39 +0530

    add links to create and list products

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { useLogout, useGetIdentity } from "@refinedev/core";
+import { useLogout, useGetIdentity, useNavigation } from "@refinedev/core";
+
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const { mutate, isLoading } = useLogout();
   const { data: identity } = useGetIdentity();
+
+  const {createUrl, listUrl} = useNavigation();
 
   return (
     <>
@@ -11,6 +15,8 @@ export const Header = () => {
         <span>Welcome, </span>
         <span>{identity?.name ?? ""}</span>
       </h2>
+      <Link to={listUrl("protected-products")}>List Products</Link>
+      <Link to={createUrl("protected-products")}>Create Products</Link>
       <button type="button" disabled={isLoading} onClick={mutate}>
         Logout
       </button>
